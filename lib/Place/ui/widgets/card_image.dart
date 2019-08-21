49f58431b32db43a2fdafd011204b31501a34f5d
jpgotopo <jpgotopo@gmail.com>
2019-08-21 07:30:22 -0500

    Acomodando los archivos según BLoC

--- lib/Place/ui/widgets/card_image.dart
+++ lib/Place/ui/widgets/card_image.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'floating_action_button_green.dart';
+import 'package:platzi_trips_app/widgets/floating_action_button_green.dart';
+
 
 class  CardImage extends StatelessWidget {
 
